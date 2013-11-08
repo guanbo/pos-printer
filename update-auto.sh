037a13d050da0b401c17712e243d01ad8919b5ca
guanbo <guanbo2002@gmail.com>
2013-11-08 18:54:00 +0800

    Add: automatic update

---
+++ update-auto.sh
@@ -0,0 +1,26 @@
+# !/bin/sh 
+
+# Description: Update POS Software automatic
+
+git fetch
+
+EXIT_CODE=`git diff origin/master --exit-code`
+
+#======================================
+#
+#Functions
+#
+#======================================
+
+function UpdateSoftware () {
+	echo "Updating Data"
+	git merge origin/master
+	# sudo sh install.sh
+	# sudo service pos-printer restart
+}
+
+# $? Exit code of git diff. exit code == 0 if no different
+
+if [[ $? ]]; then
+	UpdateSoftware
+fi
