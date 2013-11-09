ee211be7b1842b8fe3b1585fffe5a2b6a4e6f7a6
guanbo <guanbo2002@gmail.com>
2013-11-09 12:03:21 +0800

    Update: auto update

--- update-auto.sh
+++ update-auto.sh
@@ -13,14 +13,15 @@ DIFF_FILE_COUNT=`git diff --name-status master..origin/master |wc -l`
 #======================================
 
 function UpdateSoftware () {
-	echo "Updating Data"
+	echo "Updating $DIFF_FILE_COUNT Files "
 	git merge origin/master
-	# sudo sh install.sh
-	# sudo service pos-printer restart
+	ps -ef | grep keyboard-input.py | grep -v grep | awk '{print $2}'|sudo xargs kill -9 
+	sudo sh install.sh
+	sudo service pos-printer restart
 }
 
 # $? Exit code of git diff. exit code == 0 if no different
 
-if [[ $DIFF_FILE_COUNT ]]; then
+if [ $DIFF_FILE_COUNT -gt 0 ]; then
 	UpdateSoftware
 fi
