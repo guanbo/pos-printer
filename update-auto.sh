da6710f2e9aaf7230fb8714987a7a346ba8ef02c
guanbo <guanbo2002@gmail.com>
2013-11-09 07:49:11 +0000

    Update: auto update

--- update-auto.sh
+++ update-auto.sh
@@ -7,8 +7,8 @@ BASEPATH=$(dirname $0)
 cd $BASEPATH
 
 git fetch
-
 DIFF_FILE_COUNT=`git diff --name-status master..origin/master |wc -l`
+
 
 #======================================
 #
@@ -16,20 +16,20 @@ DIFF_FILE_COUNT=`git diff --name-status master..origin/master |wc -l`
 #
 #======================================
 
-function UpdateSoftware () {
+# function UpdateSoftware () {
+# }
+
+# $DIFF_FILE_COUNT Exit code of git diff. exit code == 0 if no different
+echo $DIFF_FILE_COUNT
+
+if [ $DIFF_FILE_COUNT -gt 0 ]; then
+	# UpdateSoftware
 	echo "Updating $DIFF_FILE_COUNT Files "
 	git merge origin/master
 	sudo sh install.sh
 	sudo ps -ef | grep keyboard-input.py | grep -v grep | awk '{print $2}'|sudo xargs kill -9 
 	sleep 3
-	sudo service pos-printer stop
-	sudo service pos-printer start
-}
-
-# $? Exit code of git diff. exit code == 0 if no different
-
-if [ $DIFF_FILE_COUNT -gt 0 ]; then
-	UpdateSoftware
+	sudo service pos-printer restart
 else
 	echo "Already last version!"
 fi
