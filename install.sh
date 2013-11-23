ce25d3f1445341cdf6ec8e274b395743c2a67853
guanbo <guanbo2002@gmail.com>
2013-11-23 08:19:40 +0000

    Update: Dynmaic select printer

--- install.sh
+++ install.sh
@@ -5,17 +5,20 @@
 echo "Install POS Printer"
 echo "..."
 
-#check for root user 
+# check for root user 
 if [ `whoami` != "root" ] ; then
 	echo "You must run this script as root. Sorry!"
 	exit 1
 fi
 
+# install program 
 INSTALL_DIR="/usr/local/bin/pos-printer"
 mkdir -p $INSTALL_DIR
 cp *.py $INSTALL_DIR
 chmod 755 $INSTALL_DIR/*.py
+cp adapter-printers /etc/
 
+# install startup script
 INIT_SCRIPT_FILE="pos-printer"
 INIT_SCRIPT="/etc/init.d/"$INIT_SCRIPT_FILE
 cp pos-printer.init.sh $INIT_SCRIPT
--- print.py
+++ print.py
@@ -22,17 +22,59 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
-import subprocess, sys
+import subprocess, sys, os
 from escpos import *
+from lsusb import *
 
-# upstream_data = open("gb2312.txt").read()
+prefix = "/sys/bus/usb/devices/"
+adapters = "/etc/adapter-printers"
+
+def read_usb():
+	for dirent in os.listdir(prefix):
+		#print dirent,
+		if not dirent[0:3] == "usb":
+			continue
+		usbdev = UsbDevice(None, 0)
+		usbdev.read(dirent)
+		usbdev.readchildren()
+        # print usbdev
+        return usbdev
+
+def usbprinter():
+    # "find usb printer"
+    usbdev = read_usb()
+    printers = []
+    with open(adapters) as f:
+        printers = f.read().splitlines()
+    for printer_name in printers:
+        # print "Printer:", printer_name
+        dev = usbdev.search(printer_name)
+        if dev: 
+            in_ep=0x82
+            out_ep=0x01
+            interface = dev.interfaces[0];
+            for ep in interface.eps:
+                if ep.direction == "in":
+                    in_ep=ep.epaddr
+                else:
+                    out_ep=ep.epaddr
+            # print "Found dev:", dev.name, dev.vid, dev.pid, in_ep, out_ep
+            return printer.Usb(dev.vid, dev.pid, 0, in_ep, out_ep)
+        
+Epson = usbprinter()
+# Epson.barcode('1324354657687','EAN13',64,2,'','')
+# Epson.text("\x1b\x44\x10\x18"+chr(0))
+# 
+# cols = ['coloum1', 'coloum2', 'coloum3']
+# for col in cols:
+#     Epson.text(col)
+#     Epson.control('ht')
+# 
+# Epson.text('\ntest done\n\n')
+# Epson.control('lf')
+
 input_data = sys.stdin.read()
-# input_data = open("utf8.txt").read()
 upstream_data = input_data.decode('utf-8').encode('gb2312')
-# print 'print subprocess:', upstream_data
-
-# Epson = printer.Usb(0x1cb0,0x0003)
-Epson = printer.Usb(0x8866,0x0100, 0, 0x82, 0x02)
 Epson.set()
 Epson.text(upstream_data)
 
